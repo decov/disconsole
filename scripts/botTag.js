6a2155d86834a568829085145e0c60fedf7acee3
dec <90540827+decov@users.noreply.github.com>
2022-09-23 14:14:53 -0300

    Create botTag.js

---
+++ scripts/botTag.js
@@ -0,0 +1,23 @@
+window.webpackChunkdiscord_app.push([[ Math.random() ], {}, (req) => {
+    for (const m of Object.keys(req.c).map((x) => req.c[x].exports).filter((x) => x)) {
+        if (m.default && m.default.getCurrentUser !== undefined) {
+            return m.default.getCurrentUser().bot = true;
+        }
+        
+        if (m.getCurrentUser !== undefined) {
+            return m.getCurrentUser().bot = true;
+        }
+    }
+}]);
+
+window.webpackChunkdiscord_app.push([[ Math.random() ], {}, (req) => {
+    for (const m of Object.keys(req.c).map((x) => req.c[x].exports).filter((x) => x)) {
+        if (m.default && m.default.getCurrentUser !== undefined) {
+            return m.default.getCurrentUser().isVerifiedBot = () => true;
+        }
+        
+        if (m.getCurrentUser !== undefined) {
+            return m.getCurrentUser().isVerifiedBot = () => true;
+        }
+    }
+}]);
