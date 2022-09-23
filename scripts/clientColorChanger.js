46a92a56e2b0be9e14f974728164bd39da46bd18
dec <90540827+decov@users.noreply.github.com>
2022-09-23 14:16:04 -0300

    Create clientColorChanger.js

---
+++ scripts/clientColorChanger.js
@@ -0,0 +1,11 @@
+__SECRET_EMOTION__.injectGlobal(`
+    * {
+        --background-primary: #000000;
+        --background-secondary: #000000;
+        --background-secondary-alt: #070707ff;
+        --background-accent: #252525;
+        --background-floating: #242424ff;
+        --scrollbar-thin-track: #000000;
+        --channeltextarea-background: #151515;
+    }`
+)
